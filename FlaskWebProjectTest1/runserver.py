586036c23e9af7fbabb177da7c9fc7a26b4dd9ef
Violet <2463143134@qq.com>
2019-08-16 16:37:57 +0800

    create_app is apply

--- FlaskWebProjectTest1/runserver.py
+++ FlaskWebProjectTest1/runserver.py
@@ -3,7 +3,9 @@ This script runs the FlaskWebProjectTest1 application using a development server
 """
 
 from os import environ
-from FlaskWebProjectTest1 import app
+from FlaskWebProjectTest1 import create_app
+
+app =create_app()
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
