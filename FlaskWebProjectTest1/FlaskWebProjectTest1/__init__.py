bd634a0acf82cd258b32d6873dd3b7c1f4136f87
Violet <2463143134@qq.com>
2019-08-17 14:10:53 +0800

    flask docs flaskr

--- FlaskWebProjectTest1/FlaskWebProjectTest1/__init__.py
+++ FlaskWebProjectTest1/FlaskWebProjectTest1/__init__.py
@@ -26,7 +26,20 @@ def create_app(test_config = None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    #@app.route('/')
+    #@app.route('/hello')
+    #def hello():
+    #    return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
+
